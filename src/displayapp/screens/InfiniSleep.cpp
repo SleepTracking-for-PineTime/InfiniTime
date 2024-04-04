4426a0ec9d4b7c5ca61e42c1e7017fa36ec77d50
Schweini07 <schweinthaler@gmx.at>
2024-04-04 20:35:27 +0200

    Set up basic interface for InfiniSleep

--- src/displayapp/screens/InfiniSleep.cpp
+++ src/displayapp/screens/InfiniSleep.cpp
@@ -1,10 +1,34 @@
 #include "InfiniSleep.h"
 
+#include <components/heartrate/HeartRateController.h>
+#include <components/motion/MotionController.h>
+
 using namespace Pinetime::Applications::Screens;
 
-InfiniSleep::InfiniSleep() {
+namespace {
+  void btnTransferDataEventHandler(lv_obj_t* obj, lv_event_t event) {
+      auto* screen = static_cast<InfiniSleep*>(obj->user_data);
+
+      if (event == LV_EVENT_PRESSED) {
+          screen->StartTracking();
+      }
+  }
+}
+
+InfiniSleep::InfiniSleep(
+  Controllers::HeartRateController& heartRateController,
+  Controllers::MotionController& motionController,
+  System::SystemTask& systemTask)
+  : heartRateController {heartRateController}, motionController {motionController}, systemTask {systemTask} {
+  btn_transferData = lv_btn_create(lv_scr_act(), nullptr);
+  lv_obj_set_height(btn_transferData, 100);
+  lv_obj_align(btn_transferData, nullptr, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_set_event_cb(btn_transferData, btnTransferDataEventHandler);
 }
 
 InfiniSleep::~InfiniSleep() {
+  lv_obj_clean(lv_scr_act());
 }
 
+void InfiniSleep::StartTracking() {
+}
--- src/displayapp/screens/InfiniSleep.h
+++ src/displayapp/screens/InfiniSleep.h
@@ -3,18 +3,38 @@
 #include "displayapp/apps/Apps.h"
 #include "displayapp/screens/Screen.h"
 #include "displayapp/Controllers.h"
+#include "systemtask/SystemTask.h"
 #include "Symbols.h"
+#include <lvgl/src/lv_core/lv_obj.h>
 
 namespace Pinetime {
-    namespace Applications {
-        namespace Screens {
+  namespace Controllers {
+    class HeartRateController;
+    class MotionController;
+  }
 
-            class InfiniSleep : public Screen {
-            public:
-                InfiniSleep();
-                ~InfiniSleep();
-            };
-        }
+  namespace Applications {
+    namespace Screens {
+
+        class InfiniSleep : public Screen {
+        public:
+          InfiniSleep(
+            Controllers::HeartRateController& heartRateController,
+            Controllers::MotionController& motionController,
+            System::SystemTask& systemTask
+          );
+          ~InfiniSleep();
+
+          void StartTracking();
+        
+        private:
+          Controllers::HeartRateController& heartRateController;
+          Controllers::MotionController& motionController;
+          Pinetime::System::SystemTask& systemTask;
+
+          lv_obj_t* btn_transferData;
+        };
+    }
 
     template <>
     struct AppTraits<Apps::InfiniSleep> {
@@ -22,8 +42,8 @@ namespace Pinetime {
       static constexpr const char* icon = Screens::Symbols::bed;
 
       static Screens::Screen* Create(AppControllers& controllers) {
-        return new Screens::InfiniSleep();
+        return new Screens::InfiniSleep(controllers.heartRateController, controllers.motionController, *controllers.systemTask);
       };
     };
-    }
+  }
 }
